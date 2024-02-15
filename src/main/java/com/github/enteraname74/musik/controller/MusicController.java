6d01f0f8d75cef9ef3276ce810a82cb8fbe4a1aa
enteraname74 <noahpenin@gmail.com>
2024-02-15 23:55:28 +0100

    Add initial work on MusicController

--- src/main/java/com/github/enteraname74/musik/controller/MusicController.java
+++ src/main/java/com/github/enteraname74/musik/controller/MusicController.java
@@ -2,6 +2,7 @@ package com.github.enteraname74.musik.controller;
 
 import com.github.enteraname74.musik.domain.model.Music;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -10,7 +11,8 @@ import java.util.List;
 /**
  * Controller for managing API calls related to musics.
  */
-@RestController("/music")
+@RestController
+@RequestMapping("/music")
 public class MusicController {
     @GetMapping("/all")
     List<Music> getAllMusics() {
--- src/main/java/com/github/enteraname74/musik/domain/model/Music.java
+++ src/main/java/com/github/enteraname74/musik/domain/model/Music.java
@@ -1,4 +1,7 @@
 package com.github.enteraname74.musik.domain.model;
 
+/**
+ * Represent a music and its information.
+ */
 public class Music {
 }
